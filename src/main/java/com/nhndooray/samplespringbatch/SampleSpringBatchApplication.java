32425ba7b997c4fe6ef1f903ba311357ac5b8838
Byungboo Kim <>
2023-02-12 21:54:25 +0900

    add job launcher

--- src/main/java/com/nhndooray/samplespringbatch/SampleSpringBatchApplication.java
+++ src/main/java/com/nhndooray/samplespringbatch/SampleSpringBatchApplication.java
@@ -1,14 +1,24 @@
 package com.nhndooray.samplespringbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SampleSpringBatchApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SampleSpringBatchApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctxt = SpringApplication.run(SampleSpringBatchApplication.class, args);
+
+        // TODO - 02
+        // @EnableBatchProcessing
+        JobLauncher jobLauncher = ctxt.getBean(JobLauncher.class);
+        Job weeklyStatJob = ctxt.getBean("weeklyStatJob", Job.class);
+        jobLauncher.run(weeklyStatJob, new JobParametersBuilder().toJobParameters());
     }
 
 }
