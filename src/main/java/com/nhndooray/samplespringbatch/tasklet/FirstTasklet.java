555f2c0e34178263e843d48bfac9a283fc111ccf
Byungboo Kim <>
2023-07-12 22:22:35 +0900

    update flow

--- src/main/java/com/nhndooray/samplespringbatch/tasklet/FirstTasklet.java
+++ src/main/java/com/nhndooray/samplespringbatch/tasklet/FirstTasklet.java
@@ -1,5 +1,6 @@
 package com.nhndooray.samplespringbatch.tasklet;
 
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -13,12 +14,14 @@ public class FirstTasklet implements Tasklet {
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 
-        // TODO - 02 : ExecutionContext 를 획득하는 방법
-        // TODO - 03 : ExecutionContext 의 자료형
         ExecutionContext jobExecutionContext = contribution.getStepExecution().getJobExecution().getExecutionContext();
         jobExecutionContext.putInt("counter", 1);
 
         System.out.println("FirstTasklet executed. counter=" + jobExecutionContext.getInt("counter"));
+
+        // TODO 02 - exitStatus 에 상태에 따라서 조건 분기
+        contribution.setExitStatus(ExitStatus.FAILED);
+
         return RepeatStatus.FINISHED;
     }
 }
--- src/main/java/com/nhndooray/samplespringbatch/tasklet/SampleTaskletJobConfig.java
+++ src/main/java/com/nhndooray/samplespringbatch/tasklet/SampleTaskletJobConfig.java
@@ -6,13 +6,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.config.Task;
 import org.springframework.transaction.PlatformTransactionManager;
 
-// TODO - 01 : 구성
+
 @RequiredArgsConstructor
 @Configuration
 public class SampleTaskletJobConfig {
@@ -20,31 +18,32 @@ public class SampleTaskletJobConfig {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager transactionManager;
 
+    // TODO - 01 : FlowBuilder 구성
     @Bean
-    public Job sampleTaskletJob(){
+    public Job sampleTaskletJob() {
         return new JobBuilder("sampleTaskletJob", jobRepository)
-                .start(firstStep(null))
-                .next(secondStep(null))
-                .next(thirdStep(null))
+                .start(firstStep(null)).on("FAILED").to(secondStep(null)).on("*").end()
+                .from(firstStep(null)).on("*").to(thirdStep(null)).on("*").end()
+                .end()
                 .build();
     }
 
     @Bean
-    public Step firstStep(FirstTasklet firstTasklet){
+    public Step firstStep(FirstTasklet firstTasklet) {
         return new StepBuilder("firstStep", jobRepository)
                 .tasklet(firstTasklet, transactionManager)
                 .build();
     }
 
     @Bean
-    public Step secondStep(SecondTasklet secondTasklet){
+    public Step secondStep(SecondTasklet secondTasklet) {
         return new StepBuilder("secondStep", jobRepository)
                 .tasklet(secondTasklet, transactionManager)
                 .build();
     }
 
     @Bean
-    public Step thirdStep(ThirdTasklet thirdTasklet){
+    public Step thirdStep(ThirdTasklet thirdTasklet) {
         return new StepBuilder("thirdStep", jobRepository)
                 .tasklet(thirdTasklet, transactionManager)
                 .build();
